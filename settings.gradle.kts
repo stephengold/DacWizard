d6bc53289fa80fef3a418345b8c2ce1893586f2d
stephengold <sgold@sonic.net>
2025-08-31 18:59:57 -0700

    settings.gradle.kts:  work around Gradle issue 32222

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,12 +4,12 @@ rootProject.name = "DacWizard"
 
 dependencyResolutionManagement {
     repositories {
-        //mavenLocal() // to find libraries installed locally
         mavenCentral() // to find libraries released to the Maven Central repository
         maven {
             name = "Central Portal Snapshots"
             url = uri("https://central.sonatype.com/repository/maven-snapshots/")
         }
+        //mavenLocal() // to find libraries installed locally
     }
 }
 
